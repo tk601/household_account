d9b8f7a2d830d6ff65076934bf1431802c3eb117
takato <moco.05289@gmail.com>
2021-06-15 12:04:06 +0900

    heroku commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,9 +24,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-    }
-    public function boot()
-    {
         if (\App::environment('production')) {
             \URL::forceScheme('https');
         }
