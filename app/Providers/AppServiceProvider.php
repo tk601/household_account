e4023d4109cbb9c4bdc886c618060e4f6900037a
takato <moco.05289@gmail.com>
2021-06-15 12:08:30 +0900

    second commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if (\App::environment('production')) {
-            \URL::forceScheme('https');
-        }
     }
 }
